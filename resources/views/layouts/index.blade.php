08bb88ab44de65d88c026b907b244471b0debd62
Miguel Fletes <mfletesg@gmail.com>
2022-07-08 17:49:45 -0500

    Produccion

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -20,6 +20,32 @@
 
 
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+
+    	@production
+    <link rel="stylesheet" href="{{ secure_asset('plugins/jquery/jquery.min.js') }}">
+        <!-- Font Awesome -->
+	  <link rel="stylesheet" href="{{secure_asset('plugins/fontawesome-free/css/all.min.css')}}">
+
+	  <!-- Theme style -->
+	  <link rel="stylesheet" href="{{secure_asset('dist/css/adminlte.min.css')}}">
+
+	  <!-- Theme style -->
+	  <link rel="stylesheet" href="{{secure_asset('css/home3.css')}}">
+	@endproduction
+
+  	@production
+    <link rel="stylesheet" href="{{ secure_asset('plugins/jquery/jquery.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('dist/js/adminlte.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/typed.js/typed.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/glightbox/js/glightbox.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.js') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/waypoints/noframework.waypoints.js') }}">
+	@endproduction
+
 </head>
 <body>
 
@@ -52,7 +78,7 @@
 	<script src="{{asset('assets/vendor/waypoints/noframework.waypoints.js')}}" type="text/javascript"></script>
 
 
-{{-- 	@production
+	@production
     <link rel="stylesheet" href="{{ secure_asset('plugins/jquery/jquery.min.js') }}">
     <link rel="stylesheet" href="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}">
     <link rel="stylesheet" href="{{ secure_asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}">
@@ -62,7 +88,7 @@
     <link rel="stylesheet" href="{{ secure_asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}">
     <link rel="stylesheet" href="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.js') }}">
     <link rel="stylesheet" href="{{ secure_asset('assets/vendor/waypoints/noframework.waypoints.js') }}">
-	@endproduction --}}
+	@endproduction
 
 	<script src="https://kit.fontawesome.com/9b26a92da5.js" crossorigin="anonymous"></script>
 
