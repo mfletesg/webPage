7e0d955a7aa374f340d53c7eec964119a1963e2b
Miguel Fletes <mfletesg@gmail.com>
2022-07-09 17:38:46 -0500

    Produccion

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -13,29 +13,16 @@
 
   <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
-  {{-- @env --}}
-	  <!-- Font Awesome -->
-	  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
 
-	  <!-- Theme style -->
-	  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+{{--   <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/home3.css')}}">
+ --}}
 
-	  <!-- Theme style -->
-	  <link rel="stylesheet" href="{{asset('css/home3.css')}}">
 
-  {{-- @endenv --}}
-
-  @production
-    <link rel="stylesheet" href="{{ secure_asset('plugins/jquery/jquery.min.js') }}">
-        <!-- Font Awesome -->
-	  <link rel="stylesheet" href="{{secure_asset('plugins/fontawesome-free/css/all.min.css')}}">
-
-	  <!-- Theme style -->
-	  <link rel="stylesheet" href="{{secure_asset('dist/css/adminlte.min.css')}}">
-
-	  <!-- Theme style -->
-	  <link rel="stylesheet" href="{{secure_asset('css/home3.css')}}">
-	@endproduction
+  <link rel="stylesheet" href="{{secure_asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/home3.css')}}">
 
 
 
@@ -47,33 +34,26 @@
 
 
 	<!-- jQuery -->
-
-	 {{-- @env --}}
-	{{-- <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+{{-- 	<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 	<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 	<script src="{{asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
 	<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
-
-
 	<script src="{{asset('assets/vendor/typed.js/typed.min.js')}}" type="text/javascript"></script>
-
 	<script src="{{asset('assets/vendor/glightbox/js/glightbox.min.js')}}" type="text/javascript"></script>
 	<script src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}" type="text/javascript"></script>
 	<script src="{{asset('assets/vendor/swiper/swiper-bundle.min.js')}}" type="text/javascript"></script>
 	<script src="{{asset('assets/vendor/waypoints/noframework.waypoints.js')}}" type="text/javascript"></script> --}}
-{{-- 	@endenv --}}
 
-	@production
-    <link rel="stylesheet" href="{{ secure_asset('plugins/jquery/jquery.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('dist/js/adminlte.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/typed.js/typed.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/glightbox/js/glightbox.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.js') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/waypoints/noframework.waypoints.js') }}">
-	@endproduction
+  <link rel="stylesheet" href="{{ secure_asset('plugins/jquery/jquery.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('dist/js/adminlte.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/vendor/typed.js/typed.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/vendor/glightbox/js/glightbox.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.js') }}">
+  <link rel="stylesheet" href="{{ secure_asset('assets/vendor/waypoints/noframework.waypoints.js') }}">
+
 
 	<script src="https://kit.fontawesome.com/9b26a92da5.js" crossorigin="anonymous"></script>
 
